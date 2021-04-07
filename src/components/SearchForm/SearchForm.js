baa0b63b2c54dbfd5c5c2e2460353df5588e6019
Olya Salnikova <rilfayne@yandex.com>
2021-04-07 18:40:11 +0300

    feat: исправить ошибки по ревью

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -4,7 +4,7 @@ import './SearchForm.css'
 function SearchForm() {
     return (
         <form className="search__form">
-            <input className="search__input" placeholder="Введите тему новости" type="text" />
+            <input className="search__input" placeholder="Введите тему новости" type="text" required />
             <button className="search__button" type="submit">Искать</button>
         </form>
     )
